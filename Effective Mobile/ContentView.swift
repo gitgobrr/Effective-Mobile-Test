a08daedd84c1c4cf19ee66a2e78836bb8a8ac43a
Сергей <gitgobrr@gmail.com>
2022-12-07 20:55:49 +0300

    Initial Commit

---
+++ Effective Mobile/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Effective Mobile
+//
+//  Created by sergey on 07.12.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Effective Mobile/Effective_MobileApp.swift
@@ -0,0 +1,17 @@
+//
+//  Effective_MobileApp.swift
+//  Effective Mobile
+//
+//  Created by sergey on 07.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct Effective_MobileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
